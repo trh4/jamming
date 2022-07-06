9682154246e5e939ec6b53056f45c66bc3077b42
trh4 <87888366+trh4@users.noreply.github.com>
2022-07-06 12:27:41 +0300

    add pass-down results from app.js all the way to track.js and render

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,24 +1,26 @@
 import React from "react";
-import './Track.css';
+import "./Track.css";
 
 class Track extends React.Component {
-    renderAction() {
-        if (this.props.isRemoval) {
-            return <button className="Track-action">-</button>
-        } else {
-            return <button className="Track-action">+</button>
-        }
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action">-</button>;
+    } else {
+      return <button className="Track-action">+</button>;
     }
-    render() {
-        return (
-            <div className="Track">
-                <div className="Track-information">
-                    <h3>track name</h3>
-                    <p>track artist |  track album </p>
-                </div>
-                {this.renderAction()}
-            </div>
-        )
-    };
+  }
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
 }
 export default Track;
