dd609d2d75d225b4d9fcc82b11e2e86ce5bbbed3
trh4 <87888366+trh4@users.noreply.github.com>
2022-07-07 10:05:22 +0300

    add feature to add/remove tracks from playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
-import Playlist from "../../Playlist/Playlist";
+import Playlist from "../Playlist/Playlist";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,10 +11,43 @@ class App extends React.Component {
       searchResults: [
         { name: "name1", artist: "artist1", album: "album1", id: 1 },
         { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 }
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+      ],
+      playlistName: "Sample-playlistName",
+      playlistTracks: [
+        { name: "name1IN", artist: "artist1", album: "album1", id: 4 },
+        { name: "name2IN", artist: "artist2", album: "album2", id: 5 },
       ],
     };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
+  // addTrack(track) {
+  //   let tracks = this.state.playlistTracks;
+  //   if (tracks.find((savedTrack) => savedTrack.id === track.id)) return;
+  //   tracks.push(track);
+  //   this.setState({ playlistTracks: tracks });
+  // }
+  addTrack(track) {
+    for (let playlistTrack of this.state.playlistTracks)
+      if (playlistTrack.id === track.id) return;
+    let newState = this.state.playlistTracks;
+    newState.push(track);
+    this.setState({ playlistTracks: newState });
+  }
+  // removeTrack(track) {
+  //   let iToRemove = this.state.playlistTracks.indexOf(track);
+  //   if (iToRemove === -1) return;
+  //   let newState = this.state.playlistTracks;
+  //   newState.splice(iToRemove, 1);
+  //   this.setState({ playlistTrack: newState });
+  // }
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks=tracks.filter((currentTrack)=> currentTrack.id!==track.id)
+    this.setState({playlistTracks : tracks})
+  }
+
   render() {
     return (
       <div>
@@ -24,8 +57,15 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistTracks={this.state.playlistTracks}
+              playlistName={this.state.playlistName}
+              onRemove={this.removeTrack}
+            />
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,13 +2,29 @@ import React from "react";
 import "./Track.css";
 
 class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+  addTrack(e) {
+    this.props.onAdd(this.props.track);
+  }
+  removeTrack(e){
+    this.props.onRemove(this.props.track)
+  }
   renderAction() {
     if (this.props.isRemoval) {
-      return <button className="Track-action">-</button>;
+      return <button className="Track-action" onClick={this.removeTrack}>-</button>;
     } else {
-      return <button className="Track-action">+</button>;
+      return (
+        <button className="Track-action" onClick={this.addTrack}>
+          +
+        </button>
+      );
     }
   }
+
   render() {
     return (
       <div className="Track">
